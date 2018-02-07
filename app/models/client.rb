3395a97fadd925eb74721b4200af471cd134dac5
Aurelien Allard <allard.au@gmail.com>
2018-02-07 15:09:29 -0500

    Done

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,7 @@ class Client < ApplicationRecord
   belongs_to :journey
 
   validates :name, presence: true
+  validates :mail, presence: true, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/ }
   validates :phone, format: { with: /0[1-9] (\d{2}\s){3}\d{2}/ }
 end
 
