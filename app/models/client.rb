aec6971ed66a659701bcb0109db3041d608334a6
Aurelien Allard <allard.au@gmail.com>
2018-02-23 19:09:20 +0100

    Validations for client form added

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,5 +3,5 @@ class Client < ApplicationRecord
 
   validates :name, :surname, :gender, :address, :phone, presence: true
   validates :mail, presence: true, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }
-  # validates :phone, format: { with: /0[1-9] (\d{2}\s){3}\d{2}/ }
+  validates :phone, format: { with: /0[1-9] (\d{2}\s){3}\d{2}/ }
 end
