6c92ca415f056e9a49ecb22eb812c087e39ddf58
Aurelien Allard <allard.au@gmail.com>
2018-03-12 23:13:17 +0100

    Importing variable from controller to javascript

--- app/javascript/components/pricing_updates.js
+++ app/javascript/components/pricing_updates.js
@@ -2,10 +2,29 @@ function pricing_updates () {
   const dateChange = document.getElementById("date-change");
   dateChange.addEventListener("click", (event) => {
     if (start_date != null && end_date != null) {
-      console.log(end_date.diff(start_date, 'days'));
+      var diff = end_date.diff(start_date, 'days');
+      var price;
+      var people_count = <% @journey.people_count %>;
+      switch(diff) {
+        case 0:
+          price = 90;
+          break;
+        case 1:
+          price = 120;
+          break;
+        case 2:
+          price = 170;
+          break;
+        default:
+          price = 65;
+          break;
+      }
+      document.getElementById("display-current-price").innerHTML = price + "€";
+      console.log(people_count);
     } else {
       console.log("No dates");
     }
+
   });
 }
 
