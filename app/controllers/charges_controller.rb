798c31fc2782e3f3ac180d7d2df8e97f5099308b
Aurelien Allard <allard.au@gmail.com>
2018-04-09 15:24:21 +0200

    Rearrangement of status

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,27 +8,27 @@ class ChargesController < ApplicationController
   end
 
   def create
-    customer = Stripe::Customer.create(
-      source: params[:stripeToken],
-      email:  params[:stripeEmail]
-    )
+    # customer = Stripe::Customer.create(
+    #   source: params[:stripeToken],
+    #   email:  params[:stripeEmail]
+    # )
 
-    charge = Stripe::Charge.create(
-      customer:     customer.id,   # Should store this customer id and re-use it.
-      amount:       100, #@amount,
-      description:  "Paiement de séjour",
-      currency:     'eur'
-    )
+    # charge = Stripe::Charge.create(
+    #   customer:     customer.id,   # Should store this customer id and re-use it.
+    #   amount:       100, #@amount,
+    #   description:  "Paiement de séjour",
+    #   currency:     'eur'
+    # )
 
     @journey.detail.update(payment: charge.to_json, state: 'paid')
     @journey.update(status: "Paid")
 
-    redirect_to pages_confirmation_path
+    # redirect_to pages_confirmation_path
     # redirect_to journey_charges_path(@journey)
 
-  rescue Stripe::CardError => e
-    flash[:alert] = e.message
-    redirect_to new_journey_charge_path(@journey)
+  # rescue Stripe::CardError => e
+  #   flash[:alert] = e.message
+  #   redirect_to new_journey_charge_path(@journey)
 end
 
 private
